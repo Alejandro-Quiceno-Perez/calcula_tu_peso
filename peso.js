65db0675a7db1d11e37ae335733fe7806e6d5948
Alejandro Quiceno Pérez <alejo.perez2905@gmail.com>
2022-07-05 00:45:11 -0500

    Se dhizo todo el diseño visual, aun falta el diseño responsivo

---
+++ peso.js
@@ -0,0 +1,21 @@
+const mercurio = document.getElementById('btn-mercurio');
+console.log(mercurio.value)
+function apretar() {
+
+    if (mercurio == true) {
+        console.log(mercurio.value)
+    }
+}
+/*
+var usuario = prompt('Cuanto Pesas');
+var peso = parseFloat(usuario)
+var g_tierra = 9.8;
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+var peso_final;
+
+peso_final = peso * g_jupiter;
+peso_final = peso_final;
+
+document.write(`Tu peso es de <strong> ${peso_final} kg </strong>`);
+*/
